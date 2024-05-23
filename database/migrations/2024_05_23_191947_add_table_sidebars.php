85bd8c313de30fb700f200ca9549d4d1a32a40d2
joao.vdsm <joao.vdsmg@gmail.com>
2024-05-23 17:21:23 -0300

    add basic migrations

---
+++ database/migrations/2024_05_23_191947_add_table_sidebars.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up(): void {
+    Schema::create('sidebars', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->string('icon')->nullable(true);
+      $table->string('slug');
+      $table->integer('affiliate_id');
+      $table->string('url');
+      $table->string('client_id')->default(0); // 0 = Todos | ? = client_id
+      $table->string('permission');
+      $table->integer('order');
+      $table->timestamps();
+    });
+  }
+
+  public function down(): void {
+    Schema::dropIfExists('sidebars');
+  }
+};
---
+++ database/migrations/2024_05_23_201545_add_table_tasks.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// Models
+use App\Models\Module;
+use App\Models\User;
+
+return new class extends Migration {
+  public function up(): void {
+    Schema::create('tasks', function (Blueprint $table) {
+      $table->id();
+      $table->string('title');
+      $table->foreignIdFor(Module::class);
+      $table->timestamp('initial_dt')->nullable();
+      $table->timestamp('expected_dt')->nullable();
+      $table->foreignIdFor(User::class);
+      $table->string('description');
+      $table->integer('situation');
+      $table->timestamps();
+    });
+  }
+
+  public function down(): void {
+    Schema::dropIfExists('tasks');
+  }
+};
