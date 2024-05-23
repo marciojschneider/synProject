6c4b913586653c3777e1231bfb6e2ceddba1f436
joao.vdsm <joao.vdsmg@gmail.com>
2024-05-23 15:53:47 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# synProject
-Synergya Platafor | Laravel - Bootstrap 5
+
+# Synergya Plataform
+
+Protótipo de um novo ambiente para o sistema: 
+
+ - Refatorar uma versão antiga da mesma plataforma;
+ - Melhorar o desempenho da versão em questão; 
+ - Simplificação de layout; 
+ - Melhor distribuição de recursos para a plataforma;
+ - Unificação de template (WEB/Mobile).
+
+
+
+## Funcionalidades
+
+- Layout responsivo;
+- Dark & Light;
+- Variedade de Layout (withNav, noNav, withSide...);
+- CleanUI.
+
+
+## Roadmap
+
+- Analise básica das necessidades do sistema (protótipo)
+- Definição das tabelas e campos necessários.
+- Criação das migrations e seeders necessárias.
+- Adesão de telas referências p/ o projeto.
+- Implementação dos CRUD's iniciais.
+
+
+## Stack utilizada
+
+**Front-end:** Blade | Bootstrap
+
+**Back-end:** Laravel
+
+**Database** MySQL
+
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\language\LanguageController;
+use App\Http\Controllers\pages\HomePage;
+use App\Http\Controllers\pages\Page2;
+use App\Http\Controllers\pages\MiscError;
+use App\Http\Controllers\authentications\LoginBasic;
+use App\Http\Controllers\authentications\RegisterBasic;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Main Page Route
+Route::get('/', [HomePage::class, 'index'])->name('pages-home');
+Route::get('/page-2', [Page2::class, 'index'])->name('pages-page-2');
+
+// locale
+Route::get('lang/{locale}', [LanguageController::class, 'swap']);
+
+// pages
+Route::get('/pages/misc-error', [MiscError::class, 'index'])->name('pages-misc-error');
+
+// authentication
+Route::get('/auth/login-basic', [LoginBasic::class, 'index'])->name('auth-login-basic');
+Route::get('/auth/register-basic', [RegisterBasic::class, 'index'])->name('auth-register-basic');
