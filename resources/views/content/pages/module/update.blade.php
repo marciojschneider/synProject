fab8b1d7cceaabcdd325b2a429f65ec06f2b4632
joao.vdsm <joao.vdsmg@gmail.com>
2024-05-30 19:07:57 -0300

    make update screen and logic

--- resources/views/content/pages/module/update.blade.php
+++ resources/views/content/pages/module/update.blade.php
@@ -14,16 +14,17 @@
     <div class="card mb-4">
       {{-- <h5 class="card-header">Novo chamado</h5> --}}
       <div class="card-body">
-        <form method="POST" action="{{ route('sys-module-update') }}">
+        <form method="POST" action="{{ route('sys-module-update', $module->id) }}">
           @csrf
 
           <div class="row mb-4">
             <div class="col-sm-4">
               <div class="col-md-12">
                 <label for="sidebar" class="form-label">Módulo</label>
-                <select id="sidebar" name="sidebar" class="form-select">
+                <select id="sidebar" name="sidebar" class="form-select" disabled>
                   @foreach ($sidebars as $sidebar)
-                    <option value="{{ $sidebar->id }}" {{ str_contains($module->id, $sidebar->id) ? 'selected' : '' }}>
+                    <option value="{{ $sidebar->id }}"
+                      {{ str_contains($module->slug, $sidebar->slug) ? 'selected' : '' }}>
                       {{ $sidebar->name }}</option>
                   @endforeach
                 </select>
@@ -33,7 +34,9 @@
                 <label for="profile" class="form-label">Perfis</label>
                 <select id="profile" name="profile" class="form-select">
                   @foreach ($profiles as $profile)
-                    <option value="{{ $profile->id }}">{{ $profile->name }}</option>
+                    <option value="{{ $profile->id }}"
+                      {{ str_contains($permissions->profile_id, $profile->id) ? 'selected' : '' }}>{{ $profile->name }}
+                    </option>
                   @endforeach
                 </select>
               </div>
@@ -42,7 +45,7 @@
             <div class="col-sm-8">
               <label for="description" class="form-label">Descrição</label>
               <textarea class="form-control" placeholder="Descreva o que precisa ser feito no chamado em questão." id="description"
-                name="description" maxlength="250" rows="4"></textarea>
+                name="description" maxlength="250" rows="4"> {{ $module->description }} </textarea>
             </div>
           </div>
 
@@ -54,28 +57,32 @@
               </span>
 
               <div class="form-check ">
-                <input class="form-check-input" type="checkbox" name="listCheck" />
+                <input class="form-check-input" type="checkbox" name="listCheck"
+                  {{ $permissions->list == 1 ? 'checked' : '' }} />
                 <label class="form-check-label" for="listCheck">
                   Listagem
                 </label>
               </div>
 
               <div class="form-check ">
-                <input class="form-check-input" type="checkbox" name="createCheck" />
+                <input class="form-check-input" type="checkbox" name="createCheck"
+                  {{ $permissions->create == 1 ? 'checked' : '' }} />
                 <label class="form-check-label" for="createCheck">
                   Criação
                 </label>
               </div>
 
               <div class="form-check ">
-                <input class="form-check-input" type="checkbox" name="editCheck" />
-                <label class="form-check-label" for="editCheck">
+                <input class="form-check-input" type="checkbox" name="updateCheck"
+                  {{ $permissions->update == 1 ? 'checked' : '' }} />
+                <label class="form-check-label" for="updateCheck">
                   Edição
                 </label>
               </div>
 
               <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="deleteCheck" />
+                <input class="form-check-input" type="checkbox" name="deleteCheck"
+                  {{ $permissions->delete == 1 ? 'checked' : '' }} />
                 <label class="form-check-label" for="deleteCheck">
                   Remoção
                 </label>
--- resources/views/livewire/module-list.blade.php
+++ resources/views/livewire/module-list.blade.php
@@ -56,7 +56,8 @@
                   <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i
                       class="bx bx-dots-vertical-rounded"></i></button>
                   <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#"><i class="bx bx-edit-alt me-1"></i> Editar</a>
+                    <a class="dropdown-item" href="{{ route('sys-module-update', $row->id) }}"><i
+                        class="bx bx-edit-alt me-1"></i> Editar</a>
 
                     <form method="POST" action="#">
                       @csrf
