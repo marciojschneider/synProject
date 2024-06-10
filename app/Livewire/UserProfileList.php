1f693f27a0f30ab51bc68987f03affc787ed47bf
joao.vdsm <joao.vdsmg@gmail.com>
2024-06-10 14:08:18 -0300

    more adjusts in structure archives

---
+++ app/Livewire/UserProfileList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UserProfileList extends Component
+{
+    public function render()
+    {
+        return view('livewire.user-profile-list');
+    }
+}
